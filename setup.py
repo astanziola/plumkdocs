8d84cdcda3f06bd349b3c623b59fbf370b59810a
Antonio Stanziola <stanziola.antonio@gmail.com>
2022-11-23 17:05:38 +0000

    black

--- setup.py
+++ setup.py
@@ -2,21 +2,24 @@ from setuptools import find_packages, setup
 import io
 import os
 
+
 def read(*paths, **kwargs):
-  content = ""
-  with io.open(
-    os.path.join(os.path.dirname(__file__), *paths),
-    encoding=kwargs.get("encoding", "utf8"),
-  ) as open_file:
-    content = open_file.read().strip()
-  return content
+    content = ""
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *paths),
+        encoding=kwargs.get("encoding", "utf8"),
+    ) as open_file:
+        content = open_file.read().strip()
+    return content
+
 
 def read_requirements(path):
-  return [
-    line.strip()
-    for line in read(path).split("\n")
-    if not line.startswith(('"', "#", "-", "git+"))
-  ]
+    return [
+        line.strip()
+        for line in read(path).split("\n")
+        if not line.startswith(('"', "#", "-", "git+"))
+    ]
+
 
 setup(
     packages=find_packages(include=["plumkdocs*"]),
